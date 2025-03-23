69bb4728f53a1c3fbc8eb5109e60434b6d41183e
dpieve <26913187+dpieve@users.noreply.github.com>
2025-03-23 22:40:32 +0100

    fix: viewlocator linux support

--- src/client/presentation/EasyFlow/Common/ViewLocator.cs
+++ src/client/presentation/EasyFlow/Common/ViewLocator.cs
@@ -1,6 +1,13 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using System;
+using EasyFlow.Features.Pomodoro;
+using EasyFlow.Features.Report;
+using EasyFlow.Features.Settings;
+using EasyFlow.Features.Settings.Background;
+using EasyFlow.Features.Settings.FocusTime;
+using EasyFlow.Features.Settings.HomeSettings;
+using EasyFlow.Features.Settings.Notifications;
+using EasyFlow.Features.Settings.Tags;
 using System.Collections.Generic;
 using System.ComponentModel;
 
@@ -8,30 +15,42 @@ namespace EasyFlow.Common;
 
 public class ViewLocator : IDataTemplate
 {
-    private readonly Dictionary<object, Control> _controlCache = [];
+    private readonly Dictionary<string, Control> _controlCache = [];
 
     public Control Build(object? data)
     {
-        var fullName = data?.GetType().FullName;
-        if (fullName is null)
+        if (data is null)
         {
-            return new TextBlock { Text = "Data is null or has no name." };
+            return new TextBlock { Text = "Data is null." };
         }
 
-        var name = fullName.Replace("ViewModel", "View");
-        var type = Type.GetType(name);
-        if (type is null)
+        string? name = data.GetType().Name;
+
+        if (name is null)
         {
-            return new TextBlock { Text = $"No View For {name}." };
+            return new TextBlock { Text = "Name is null." };
         }
 
-        if (!_controlCache.TryGetValue(data!, out var res))
+        if (_controlCache.TryGetValue(name, out var res))
         {
-            res ??= (Control)Activator.CreateInstance(type)!;
-            _controlCache[data!] = res;
+            return res;
         }
 
-        res.DataContext = data;
+        res = name switch
+        {
+            nameof(PomodoroViewModel) => new PomodoroView() { DataContext = data },
+            nameof(ReportViewModel) => new ReportView() { DataContext = data },
+            nameof(BackgroundViewModel) => new BackgroundView() { DataContext = data },
+            nameof(FocusTimeViewModel) => new FocusTimeView() { DataContext = data },
+            nameof(HomeSettingsViewModel) => new HomeSettingsView() { DataContext = data },
+            nameof(NotificationsViewModel) => new NotificationsView() { DataContext = data },
+            nameof(TagsViewModel) => new TagsView() { DataContext = data },
+            nameof(SettingsViewModel) => new SettingsView() { DataContext = data },
+            _ => new TextBlock { Text = $"No View For {name}." },
+        };
+
+        _controlCache.Add(name, res);
+
         return res;
     }
 
