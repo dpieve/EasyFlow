6a488a72113346e191a4a8dfd444fa9f0e8c7ec0
dpieve <26913187+dpieve@users.noreply.github.com>
2025-03-23 19:17:23 +0100

    feat: linux support

--- src/client/presentation/EasyFlow.Linux/Program.cs
+++ src/client/presentation/EasyFlow.Linux/Program.cs
@@ -6,38 +6,66 @@ using ReactiveUI;
 using Splat;
 using Splat.Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
+using System.Threading;
 
 namespace EasyFlow.Linux;
 
 internal sealed class Program
 {
     [STAThread]
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        AppInit(args);
+        var host = BuildHost();
+
+        var builder = BuildAvaloniaApp();
+        if (args.Contains("--drm"))
+        {
+            SilenceConsole();
+            return builder.StartLinuxDrm(args: args, card: null, scaling: 1.0);
+        }
+
+        return builder.StartWithClassicDesktopLifetime(args);
     }
 
-    private static void AppInit(string[] args)
-    {
-        _ = Host.CreateDefaultBuilder()
-                .ConfigureServices((_, services) =>
-                {
-                    services.UseMicrosoftDependencyResolver();
-                    var resolver = Locator.CurrentMutable;
-                    resolver.InitializeSplat();
-                    resolver.InitializeReactiveUI();
-
-                    services
-                        .AddPlatform()
-                        .AddPresentation();
-                })
-                .Build();
-
-        AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
-            .UseReactiveUI()
-            .StartWithClassicDesktopLifetime(args);
+            .UseReactiveUI();
+
+    public static IHost BuildHost()
+    {
+        return Host.CreateDefaultBuilder()
+            .UseDefaultServiceProvider(options =>
+            {
+                options.ValidateScopes = true;
+                options.ValidateOnBuild = true;
+            })
+            .ConfigureServices((_, services) =>
+            {
+                services.UseMicrosoftDependencyResolver();
+
+                var resolver = Locator.CurrentMutable;
+                resolver.InitializeSplat();
+                resolver.InitializeReactiveUI();
+
+                services
+                    .AddPlatform()
+                    .AddPresentation();
+            })
+            .Build();
+    }
+
+    private static void SilenceConsole()
+    {
+        new Thread(() =>
+        {
+            Console.CursorVisible = false;
+            while (true)
+                Console.ReadKey(true);
+        })
+        { IsBackground = true }.Start();
     }
 }
