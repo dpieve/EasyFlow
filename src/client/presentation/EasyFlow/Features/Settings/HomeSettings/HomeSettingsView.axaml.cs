46b35cbf7ed45c84244951b939e9d6799d221f5f
dpieve <26913187+dpieve@users.noreply.github.com>
2025-03-25 21:40:04 +0100

    feat: add power off on Linux

--- src/client/presentation/EasyFlow/Features/Settings/HomeSettings/HomeSettingsView.axaml.cs
+++ src/client/presentation/EasyFlow/Features/Settings/HomeSettings/HomeSettingsView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System;
 
 namespace EasyFlow.Features.Settings.HomeSettings;
 
@@ -7,5 +8,16 @@ public partial class HomeSettingsView : UserControl
     public HomeSettingsView()
     {
         InitializeComponent();
+
+        if (OperatingSystem.IsLinux())
+        {
+            PowerOffButton.IsVisible = true;
+        }
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+
+        Environment.Exit(0);
     }
 }
