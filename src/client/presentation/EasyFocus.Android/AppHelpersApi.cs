5837e7f90cb3da20bd7052674f6fcc96fe17576f
dpieve <26913187+dpieve@users.noreply.github.com>
2025-05-08 22:11:36 +0200

    fix: android support

---
+++ src/client/presentation/EasyFocus.Android/AppHelpersApi.cs
@@ -0,0 +1,46 @@
+﻿using EasyFocus.Domain.Services;
+using System.Threading.Tasks;
+
+namespace EasyFocus.Android;
+
+public class AppHelpersApi : IAppHelpersApi
+{
+    public AppHelpersApi()
+    {
+    }
+
+    public Task LogValue(string message)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ShowNotification(string title, string message)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task PlayAudio(string audioFileName, int volume)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task SetBrowserTitle(string title)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> OpenUrl(string url)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task<string> GetStorageItem(string key)
+    {
+        return Task.FromResult(string.Empty);
+    }
+
+    public Task SetStorageItem(string key, string value)
+    {
+        return Task.CompletedTask;
+    }
+}
